e9f230caac7c3e7fddf75520537300a23bacca67
Dom Shahbazi <domshahbazi@Doms-MBP.lan>
2016-03-19 18:16:25 +0000

    [PAGE-199] hooking up the content

---
+++ Homepwner/DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by Dom Shahbazi on 19/03/2016.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+    
+    var item: Item!
+    
+    let numberFormatter: NSNumberFormatter = {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .DecimalStyle
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nameField.text = item.name
+        serialNumberField.text = item.serialNumber
+        valueField.text = "\(item.valueInDollars)"
+        dateLabel.text = "\(item.dateCreated)"
+    }
+    
+}
+
+
